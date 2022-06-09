ab618e7e233d46f60d0b2b1f4ea00dd3bc1ba32c
wael <78425538+wa20@users.noreply.github.com>
2022-06-09 14:14:44 +0100

    created initial schema file and imported into server file

---
+++ server/schema/schema.js
@@ -0,0 +1,41 @@
+const { Project, Clients } = require('./dataSample')
+const {GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLSchema} = require('graphql');
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        Phone: {type: GraphQLString}
+
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            //the below argument allows us to know which client we are getting
+            args: {id: {type: GraphQLID}},
+            //the resolver is our return that takes in a parent value and an argument 'args'. when we use mongodb we use a moongoose function to get our query
+            resolve(parentValue, args){
+                //the below is only used as we currently do not have mongodb set up yet
+                return Clients.find(client => client.id === args.id);
+
+            }
+
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema ({
+    //this takes in an object with a query (in our case RootQuery above)
+    query: RootQuery
+})
+
+
+
