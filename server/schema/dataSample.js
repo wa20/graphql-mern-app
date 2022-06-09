3eef68f0503ce786007ae8213caece29c5a16e3e
wael <78425538+wa20@users.noreply.github.com>
2022-06-09 14:15:26 +0100

    added datasample to use

---
+++ server/schema/dataSample.js
@@ -0,0 +1,81 @@
+
+// sample data used for educational purposes   
+// Projects
+const Projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'some sort of description of application written into this space, right here!',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'some sort of description of application written into this space, right here!',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'some sort of description of application written into this space, right here!',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'some sort of description of application written into this space, right here!',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'some sort of description of application written into this space, right here!',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const Clients = [
+    {
+      id: '1',
+      name: 'Tony Stark',
+      email: 'ironman@gmail.com',
+      phone: '343-567-4333',
+    },
+    {
+      id: '2',
+      name: 'Natasha Romanova',
+      email: 'blackwidow@gmail.com',
+      phone: '223-567-3322',
+    },
+    {
+      id: '3',
+      name: 'Thor Odinson',
+      email: 'thor@gmail.com',
+      phone: '324-331-4333',
+    },
+    {
+      id: '4',
+      name: 'Steve Rogers',
+      email: 'steve@gmail.com',
+      phone: '344-562-6787',
+    },
+    {
+      id: '5',
+      name: 'Bruce Banner',
+      email: 'bruce@gmail.com',
+      phone: '321-468-8887',
+    },
+  ];
+  
+  module.exports = { Projects, Clients };
