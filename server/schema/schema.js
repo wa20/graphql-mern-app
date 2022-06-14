c3be4b329e19185ecc6d35f847d82b596a1ed97d
wael <78425538+wa20@users.noreply.github.com>
2022-06-14 22:10:00 +0100

    imported models, also added first part of mutation so now able to add new clients

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 const { projects, clients } = require('./dataSample.js')
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql');
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull} = require('graphql');
+
+//mongoose models
+const Project = require('../models/Projects')
+const Client = require('../models/Client')
 
 //Client Type
 const ClientType = new GraphQLObjectType({
@@ -27,13 +31,16 @@ const ProjectType = new GraphQLObjectType({
             resolve(parent, args){
                 //client is a child of project
                 // here we find the matching client and project ids
-                return clients.find(client => client.id === parent.clientId)
+                // return clients.find(client => client.id === parent.clientId)
+                Client.findById(parent.clientId)
             }
         }
 
     })
 });
 
+
+// the RootQuery enables you to pull data from the database
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -44,7 +51,9 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(ClientType),
             //do not need args as we do below as we are getting all clients and do need an id
             resolve(parent, args){
-                return clients;
+                // return clients;
+
+                return Client.find()
             }
         },
         client: {
@@ -54,7 +63,9 @@ const RootQuery = new GraphQLObjectType({
             //the resolver is our return that takes in a parent value and an argument 'args'. when we use mongodb we use a moongoose function to get our query
             resolve(parent, args){
                 //the below is only used as we currently do not have mongodb set up yet
-                return clients.find(client => client.id === args.id);
+                // return clients.find(client => client.id === args.id);
+
+                return Client.findById(args.id)
 
             }
 
@@ -65,7 +76,9 @@ const RootQuery = new GraphQLObjectType({
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args){
-                return projects;
+                // return projects;
+                //below we want to return all data so have not added any arguments 
+                return Project.find()
             }
         },
         project: {
@@ -73,7 +86,10 @@ const RootQuery = new GraphQLObjectType({
       
             args: {id: {type: GraphQLID}},
             resolve(parent, args){
-            return projects.find(project => project.id === args.id);
+            // return projects.find(project => project.id === args.id);
+
+            //below we input an argument becasue we want to find a project by ID
+            return Project.findById(args.id)
 
             }
 
@@ -85,9 +101,45 @@ const RootQuery = new GraphQLObjectType({
 })
 
 
+
+//Mutations - allows us to add, change, delete
+
+const mutation = new GraphQLObjectType({
+    name: 'Muation',
+    fields: {
+
+        //add 
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)},
+                email: {type: GraphQLNonNull(GraphQLString)},
+                phone: {type: GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                })
+
+                return client.save()
+            }
+        },
+
+        //delete
+        
+
+
+
+    }
+})
+
+
 module.exports = new GraphQLSchema ({
     //this takes in an object with a query (in our case RootQuery above)
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
 
 
