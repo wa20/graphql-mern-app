3be494f9a3fc38701b0721e5baebcafea8779122
wael <78425538+wa20@users.noreply.github.com>
2022-06-15 22:31:37 +0100

    added apollo to work with graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import Header from "./components/header/header";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <> 
-    
+    <ApolloProvider client={client}>
     <Header/>
 
     <div className="App">
       <header className="App-header">
        
-         
           hello React
   
       </header>
     </div>
     
-    
+    </ApolloProvider>
     </>
     
   );
