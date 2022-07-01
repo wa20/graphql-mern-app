d1d1d071624b087d373528175e9a103db15f5c7d
wael <78425538+wa20@users.noreply.github.com>
2022-07-01 10:50:20 +0100

    Added delete functionality, now able to remove clients on front end

--- client/src/components/clinetRow/clientRow.js
+++ client/src/components/clinetRow/clientRow.js
@@ -1,13 +1,34 @@
 import React from "react";
+//importing the use mutation hook
 import { useMutation } from "@apollo/client"; 
 import {DELETE_CLIENT} from '../../utils/mutations'
+import { GET_CLIENTS } from "../../utils/queries";
+
 
 
 export default function ClientRow({client}){
 
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         //here we tell it what it is we are trying to delete
-        variables: {id: client.id}
+        variables: {id: client.id},
+        //two methods to refresh the query, 1st is to refresh the query and 2nd is to update the cache
+        //1 - refetch query: this is ok for low level fetches, but if you are fetching alot then it will start to bog down the app
+        // refetchQueries: [{query: GET_CLIENTS}]
+
+        //2 - update the cache
+        update(cache, {data: {deleteClient}}){
+            //here we get the query from the cache
+            const { clients } = cache.readQuery({
+                query: GET_CLIENTS
+            })
+            //below we write to cache with writeQuery
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                //below we filter out the client we want to delete.  
+                data: {clients: clients.filter(client => client.id !== deleteClient.id)}
+            })
+        }
+
     })
 
     return (
