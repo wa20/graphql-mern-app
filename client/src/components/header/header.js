36c4d6d7cf7a78a1743f46778a2bb0f7fa5f3a3d
wael <78425538+wa20@users.noreply.github.com>
2022-07-01 09:32:46 +0100

    class amended to className

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,28 +1,28 @@
-import react from "react";
+import React from "react";
 import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-        <img src={logo} width='35' height='35' className="mr-5"/>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+        <img src={logo} width='35' height='35' className="mr-5" alt="logo"/>
  
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Menu 2</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Menu 2</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Menu 3</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Menu 3</a>
         </li>
       </ul>
     </div>
