200f5b8381565be759a5a207a82b6fb7b117c47b
wael <78425538+wa20@users.noreply.github.com>
2022-07-01 11:41:52 +0100

    added inmemory cache to app js to merge exisiting cache

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,31 @@ import Header from "./components/header/header";
 import Clients from "./components/clients/clients";
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
 
+const cache = new InMemoryCache({
+  typePolicies: {
+      Query: {
+          fields: {
+              clients: {
+                  merge(exisitng, incoming) {
+                      return incoming
+                  }
+              },
+
+              projects: {
+                merge(exisitng, incoming) {
+                    return incoming
+                 }
+              }
+      }
+    }
+  }
+})
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache()
+  cache,
+  // cache: new InMemoryCache()
 })
 
 function App() {
