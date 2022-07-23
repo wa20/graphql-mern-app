210ecbe2f75d04f360a56c243a383c0d39948865
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 15:40:27 +0100

    updated project to pull in project and connected client data

--- client/src/pages/projects/project.js
+++ client/src/pages/projects/project.js
@@ -1,14 +1,53 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import ClintInfo from "../../components/clientInfo/clientInfo";
+//useParams is used here baceasue we want to get the id from the url of page
 
-export default function Projects(){
+import Spinner from "../../components/spinner/spinner";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECT } from "../../utils/queries"; 
 
-    return (
-        <>
-         Projects
-        </>
+export default function Projects() {
+  const { id } = useParams();
 
-    )
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
 
+  if (loading) return <Spinner />;
+  if (error) return <p>Error</p>;
 
+  return (
+    <>
+      <div className="component mt-5">
+        {!loading && !error && (
+          // <div className="mx-auto w-75 card p-5">
+          //     <Link to='/' className="btn btn-light btn-sm w-25 d-inline ms-auto">Back</Link>
+          //     <h1>{data.project.name}</h1>
+          //     <p>{data.project.descripton}</p>
 
+          //     <h5 className="mt-3">Project Status</h5>
+          //     <p className="lead">{data.project.status}</p>
+          // </div>
+
+          <div class="card text-center">
+            <div class="card-header">Project</div>
+            <div class="card-body">
+              <h5 class="card-title">{data.project.name}</h5>
+              <p class="card-text">{data.project.description}</p>
+              <Link
+                to="/"
+                className="btn btn-primary btn-sm w-25 d-inline ms-auto">
+                Back
+              </Link>
+            </div>
+            <div class="card-footer text-muted">{data.project.status}</div>
+
+            <div className="row">
+                <ClintInfo client={data.project.client}/>
+            </div>
+            
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
