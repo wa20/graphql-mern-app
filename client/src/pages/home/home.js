930a2c7e702e44cb61c8c4d50df2c2669fa0e9d6
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 16:54:09 +0100

    updated homepage

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -3,24 +3,30 @@ import Header from "../../components/header/header";
 import Clients from "../../components/clients/clients";
 import Projects from "../../components/projects/projects";
 import AddClientBtn from "../../components/clientModal/clientModal";
-
-
-
-
+import AddProjectBtn from "../../components/projectModal/projectModal";
 
 function Home() {
   return (
     <>
+      {/* <Header /> */}
+      <div className=" d-flex align-items-center flex-column container my-5">
+        <div className="container row  mt-5">
+          <Projects />
+        </div>
+        <div className=" my-4 d-flex align-items-center flex-column container">
+          <AddProjectBtn />
+        </div>
+        
+      </div>
       
-          {/* <Header /> */}
-          <div className=" d-flex align-items-center flex-column container my-5">
-            <Projects />
-            <Clients />
-          </div>
-          <div className=" d-flex align-items-center flex-column container my-5">
-            <AddClientBtn />
-          </div>
-       
+      <span className="border-bottom mb-4 border-3"></span>
+
+
+      <Clients />
+
+      <div className=" d-flex align-items-center flex-column container my-5">
+        <AddClientBtn />
+      </div>
     </>
   );
 }
