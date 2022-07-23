369dc21757ae1bc8809795130df00173473698d1
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 12:45:36 +0100

    home page componenet added

---
+++ client/src/pages/home/home.js
@@ -0,0 +1,27 @@
+import Header from "../../components/header/header";
+import Clients from "../../components/clients/clients";
+import Projects from "../../components/projects/projects";
+import AddClientBtn from "../../components/clientModal/clientModal";
+
+
+
+
+
+function Home() {
+  return (
+    <>
+      
+          {/* <Header /> */}
+          <div className=" d-flex align-items-center flex-column container my-5">
+            <Projects />
+            <Clients />
+          </div>
+          <div className=" d-flex align-items-center flex-column container my-5">
+            <AddClientBtn />
+          </div>
+       
+    </>
+  );
+}
+
+export default Home;
