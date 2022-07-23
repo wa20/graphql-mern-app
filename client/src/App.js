81ae96ccf81c8426d17ac41a59c682613ce0447c
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 11:50:45 +0100

    project added to app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/header/header";
 import Clients from "./components/clients/clients";
+import Projects from "./components/projects/projects";
 import AddClientBtn from "./components/clientModal/clientModal";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
@@ -36,6 +37,7 @@ function App() {
         <Header />
 
         <div className=" d-flex align-items-center flex-column container my-5">
+          <Projects/>
           <Clients />
         </div>
         <div className=" d-flex align-items-center flex-column container my-5">
