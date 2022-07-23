ccd2dd121f51094a7837701b2596698c8f6c21ff
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 10:19:46 +0100

    get projects query added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,15 +1,22 @@
-
-import {gql} from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const GET_CLIENTS = gql`
-query getClients {
-  clients {
-    id
-    name
-    email
-    phone
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
   }
-}
 `;
 
-
+export const GET_PROJECTS = gql`
+  query getProjects {
+    projects {
+      id
+      name
+      status
+    }
+  }
+`;
