2556bebe4f55a672d456e42ed3459d7224191ccb
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 15:32:55 +0100

    added get single project query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,7 +16,28 @@ export const GET_PROJECTS = gql`
     projects {
       id
       name
+      description
       status
     }
   }
 `;
+
+export const GET_PROJECT = gql`
+  query getProject($id: ID!) {
+    project(id: $id){
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+    
+  }
+`;
+
+
