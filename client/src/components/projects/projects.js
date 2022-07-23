eb6b2a60beff57ff10d0467f0be9123957c754ae
wael <78425538+wa20@users.noreply.github.com>
2022-07-23 11:50:00 +0100

    project card imported to project file

---
+++ client/src/components/projects/projects.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ProjectCard from "../projectCard/projectCard";
+import Spinner from "../spinner/spinner";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECTS } from "../../utils/queries";
+
+
+
+
+export default function Projects(){
+    const { loading, error, data } = useQuery(GET_PROJECTS);
+
+    if (loading) return <Spinner/>;
+    if (error) return <p>Error</p>;
+
+
+
+    return (
+    <div>
+
+        {data.projects.length > 0 ? 
+         <div className="row">
+            {data.projects.map((project) => ( 
+                <ProjectCard key={project.id} project ={project}/>
+            ))}
+        </div>
+        : 
+        <h2>No Projects Created</h2>
+        } 
+        
+    </div>
+   )
+}
