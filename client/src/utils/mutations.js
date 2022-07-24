1105985247773816c87e85154a1fd53cc6e8f8b1
wael <78425538+wa20@users.noreply.github.com>
2022-07-24 15:07:44 +0100

    added add project and delete project mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,58 @@
 import { gql } from "@apollo/client";
 
 export const ADD_CLIENT = gql`
-mutation addClient($name: String!, $email: String!, $phone: String!) {
-  addClient(name: $name, email: $email, phone: $phone) {
-    id
-    name
-    email
-    phone
+  mutation addClient($name: String!, $email: String!, $phone: String!) {
+    addClient(name: $name, email: $email, phone: $phone) {
+      id
+      name
+      email
+      phone
+    }
   }
-}
 `;
 
 export const DELETE_CLIENT = gql`
-mutation deleteClient($id: ID! ){
-    deleteClient(id: $id){
+  mutation deleteClient($id: ID!) {
+    deleteClient(id: $id) {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+export const ADD_PROJECT = gql`
+  mutation addProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
+      id
+      name
+      description
+      status
+      client {
         id
         name
         email
         phone
-    } 
+      }
+    }
   }
-` ;
+`;
+
+export const DELETE_PROJECT = gql`
+  mutation deleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      id      
+    }
+  }
+`;
