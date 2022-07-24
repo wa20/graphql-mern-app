98203189046f34f51d603fb66d070dd16fd99332
wael <78425538+wa20@users.noreply.github.com>
2022-07-24 19:19:27 +0100

    delete button imported into project page

--- client/src/pages/projects/project.js
+++ client/src/pages/projects/project.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import ClintInfo from "../../components/clientInfo/clientInfo";
+import ClientInfo from "../../components/clientInfo/clientInfo";
 //useParams is used here baceasue we want to get the id from the url of page
 
 import Spinner from "../../components/spinner/spinner";
 import { useQuery } from "@apollo/client";
-import { GET_PROJECT } from "../../utils/queries"; 
+import { GET_PROJECT } from "../../utils/queries";
+import DeleteProjectBtn from "../../components/deleteProjectBtn/deleteProjectBtn";
 
 export default function Projects() {
   const { id } = useParams();
-
   const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
 
   if (loading) return <Spinner />;
@@ -19,15 +19,6 @@ export default function Projects() {
     <>
       <div className="component mt-5">
         {!loading && !error && (
-          // <div className="mx-auto w-75 card p-5">
-          //     <Link to='/' className="btn btn-light btn-sm w-25 d-inline ms-auto">Back</Link>
-          //     <h1>{data.project.name}</h1>
-          //     <p>{data.project.descripton}</p>
-
-          //     <h5 className="mt-3">Project Status</h5>
-          //     <p className="lead">{data.project.status}</p>
-          // </div>
-
           <div class="card text-center">
             <div class="card-header">Project</div>
             <div class="card-body">
@@ -35,16 +26,22 @@ export default function Projects() {
               <p class="card-text">{data.project.description}</p>
               <Link
                 to="/"
-                className="btn btn-primary btn-sm w-25 d-inline ms-auto">
+                className="btn btn-primary btn-sm w-25 d-inline ms-auto"
+              >
                 Back
               </Link>
+              <div className="d-grid gap-2 d-md-block">
+                <DeleteProjectBtn projectId={data.project.id} />
+              </div>
             </div>
             <div class="card-footer text-muted">{data.project.status}</div>
 
-            <div className="row">
-                <ClintInfo client={data.project.client}/>
+            <div className="row g-3">
+              <ClientInfo client={data.project.client} />
             </div>
-            
+            {/* <div className="d-flex justify-content-center align-items-center"> */}
+
+            {/* </div> */}
           </div>
         )}
       </div>
